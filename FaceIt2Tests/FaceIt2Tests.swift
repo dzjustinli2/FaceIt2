c697e210828087da9211161c494c97e3b529a2d9
Justin LI <dzjustinli2@gmail.com>
2016-07-24 13:52:56 +1000

    Initial Commit

---
+++ FaceIt2Tests/FaceIt2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  FaceIt2Tests.swift
+//  FaceIt2Tests
+//
+//  Created by justin on 24/07/2016.
+//  Copyright © 2016 justin. All rights reserved.
+//
+
+import XCTest
+@testable import FaceIt2
+
+class FaceIt2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
